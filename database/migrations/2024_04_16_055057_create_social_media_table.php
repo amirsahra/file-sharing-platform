d1cb0aa025eee82d5ed267fd0f0e466b8d96d3ad
amirsahra <amirhosein.sahra@gmail.com>
2024-04-16 10:25:21 +0430

    feat(social media): migration

---
+++ database/migrations/2024_04_16_055057_create_social_media_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('social_media', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('profile_id');
+            $table->string('name', 255);
+            $table->string('profile_url', 255)->nullable();
+            $table->string('username', 255)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('social_media');
+    }
+};
